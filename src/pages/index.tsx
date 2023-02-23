dcb74130514091923b85185199df388e685d59c2
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-02-22 22:27:21 -0300

    Cria template da aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
+import Layout from "../components/template/Layout";
 
 export default function Home() {
   return (
-    <div className={`
-    h-screen
-    bg-gradient-to-r from-green-500 to-blue-500
-    `}>
-      Admin
-    </div>
+   <Layout titulo="Página Inicial" subtitulo="Estamos construíndo um template Admin!">
+    <h3>Conteúdo!!!!</h3>
+   </Layout>
   )
 }
