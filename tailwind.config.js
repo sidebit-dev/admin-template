2fb199e50a6eda332875a6d0e6192528deb81f9b
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-02-23 00:28:24 -0300

    Aplica algum estilo ao Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     './src/pages/**/*.{js,ts.jsx,tsx}',
     './src/components/**/*.{js,ts.jsx,tsx}'
   ],
-  content: [],
+  darkMode: 'class', // 'media' or 'class'
+ // content: [ ],
+  
   theme: {
     extend: {},
   },
