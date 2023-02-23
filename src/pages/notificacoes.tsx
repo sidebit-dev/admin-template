9b578a34fd294f2f7fbf2bea5a68c90faf7927e6
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-02-23 20:17:17 -0300

    Cria principio Context API

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,10 +1,14 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hook/useAppData";
+
 
 export default function Notificacoes() {
+  const ctx = useAppData()
+
   return (
    <Layout titulo="Notificações" 
    subtitulo="Aqui você vai gerenciar as suas notificações!">
-    <h3>Conteúdo!!!!</h3>
+    <h3>{ctx.nome}</h3>
    </Layout>
   )
 }
